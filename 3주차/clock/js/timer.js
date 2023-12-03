197971fb93d45828447aa9c6b95df082a77f6c16
SinJungho <zmtls0516@naver.com>
2023-12-03 23:06:55 +0900

    타이머, 날짜 기능 완성

---
+++ 3주차/clock/js/timer.js
@@ -0,0 +1,27 @@
+let printDate = document.getElementById("date");
+let printTime = document.getElementById("time");
+let dateInfo = new Date();
+
+// date
+function getDate() {
+  let year = dateInfo.getFullYear();
+  let month = dateInfo.getMonth();
+  let date = dateInfo.getDate();
+
+  printDate.innerHTML = `${year} - ${month} - ${date}`;
+}
+
+//timer
+function getTime(params) {
+  setInterval(function() {
+    dateInfo = new Date(); //dateInfo가 실시간 업데이트를 못해서 다시 객체 참조
+    let hour = String(dateInfo.getHours()).padStart(2, "0");
+    let min = String(dateInfo.getMinutes()).padStart(2, "0");
+    let sec = String(dateInfo.getSeconds()).padStart(2, "0");
+  
+    printTime.innerText = (`${hour} : ${min} : ${sec}`);
+  }, 1000);
+}
+
+getDate();
+getTime();
