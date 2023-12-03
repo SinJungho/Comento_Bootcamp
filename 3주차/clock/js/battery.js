f73939752cbc981ca1f5c9f2e3c663acd6e2708b
SinJungho <zmtls0516@naver.com>
2023-12-03 17:47:45 +0900

    배터리 수정 및 구현 완료

--- 3주차/clock/js/battery.js
+++ 3주차/clock/js/battery.js
@@ -1,14 +1,21 @@
 let battery = document.getElementById("battery-text");
+let timer = document.getElementById("timer");
 let maxBattery = 100;
 
 function useBattery() {
   setInterval(function(){
-    console.log(maxBattery--);
-    battery.innerText = maxBattery-- + "%";
-
     if (maxBattery == 0) {
+      // maxBattery = 100;
+      timer.style.backgroundColor = "black";
       maxBattery = 100;
+    } else if(maxBattery != 0){
+      // maxBattery = 100;
+      timer.style.backgroundColor = "white";
     }
+
+    battery.innerText = maxBattery + "%";
+    maxBattery--;
+    
   }, 1000);
 }
 
